0dcf4ee6ae69be71f1a0dca74fee96a9050695c4
ElenaStepanchuk <podolyak365@gmail.com>
2022-04-22 16:59:08 +0300

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,30 @@
+// import { useSelector, useDispatch } from 'react-redux';
+// import { update, getClicksValue } from '../redux/clicksSlice';
+import css from './App.module.css';
+import React from 'react';
+import AddContacts from './AddContacts';
+
 export const App = () => {
+  // const dispatch = useDispatch();
+  // const numberOfClicks = useSelector(getClicksValue);
+
+  // console.log(update(10));
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    // <div>
+    //   <h1>Number of clicks: {numberOfClicks}</h1>
+    //   <button type="button" onClick={() => dispatch(update(5))}>
+    //     Add 5 clicks
+    //   </button>
+    //   <button type="button" onClick={() => dispatch(update(10))}>
+    //     Add 10 clicks
+    //   </button>
+    //   <button type="button" onClick={() => dispatch(update(20))}>
+    //     Add 20 clicks
+    //   </button>
+    // </div>
+    <div className={css.App}>
+      <AddContacts />
     </div>
   );
 };
---
+++ src/redux/store.js
@@ -0,0 +1,25 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import { ClicksReducer } from './clicksSlice';
+
+export const store = configureStore({
+  reducer: {
+    clicks: ClicksReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
