c55fdf880bd95346392dedb3c26d40a76cd22301
ElenaStepanchuk <podolyak365@gmail.com>
2022-05-11 21:31:58 +0300

    props

--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -2,16 +2,33 @@ import React from 'react';
 import ContactListItem from './ContactListItem';
 import css from './ContactsList.module.css';
 import PropTypes from 'prop-types';
-const ContactsList = ({ contacts, onDelContact }) => {
+import { useDispatch } from 'react-redux';
+import { remove } from '../redux/contactsSlice';
+// import { useSelector } from 'react-redux';
+const ContactsList = ({ contacts }) => {
+  const dispatch = useDispatch();
+  // const contacts = useSelector(state => state.contacts);
+  // const filter = useSelector(state => state.filter);
+  // const getFilterName = () => {
+  //   const normalizedFilter = filter.toLowerCase();
+  //   return Object.values(contacts).filter(contact => {
+  //     if (contact.name)
+  //       return contact.name.toLowerCase().includes(normalizedFilter);
+  //   });
+  // };
+  const handleDelContact = contactId => {
+    dispatch(remove(contactId));
+  };
   return (
     <ul className={css.contact__list}>
-      {contacts.map(({ id, name, number }) => (
+      {Object.values(contacts).map(({ id, name, number }) => (
         <li className={css.contact__item} key={id}>
           <ContactListItem
+            // contacts={getFilterName()}
             id={id}
             name={name}
             number={number}
-            onDelContact={onDelContact}
+            onDelContact={handleDelContact}
           />
         </li>
       ))}
