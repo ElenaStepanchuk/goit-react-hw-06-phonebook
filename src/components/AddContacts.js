c966228ff92b22c49d5b411ec413ae2acd4faa3a
ElenaStepanchuk <podolyak365@gmail.com>
2022-05-12 11:19:04 +0300

    correct props

--- src/components/AddContacts.js
+++ src/components/AddContacts.js
@@ -1,25 +1,14 @@
 import Form from './Form';
 import ContactsList from './ContactsList';
 import Filter from './Filter';
-import { useSelector } from 'react-redux';
 const AddContacts = () => {
-  const contacts = useSelector(state => state.contacts);
-  const filter = useSelector(state => state.filter);
-  const getFilterName = () => {
-    const normalizedFilter = filter.toLowerCase();
-    return Object.values(contacts).filter(contact => {
-      if (contact.name)
-        return contact.name.toLowerCase().includes(normalizedFilter);
-    });
-  };
   return (
     <>
       <h1 className="title">Phonebook</h1>
       <Form />
       <h2 className="title">Contacts</h2>
       <Filter />
-      <ContactsList contacts={getFilterName()} />
-      {/* <ContactsList /> */}
+      <ContactsList />
     </>
   );
 };
--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -2,36 +2,32 @@ import React from 'react';
 import ContactListItem from './ContactListItem';
 import css from './ContactsList.module.css';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { remove } from '../redux/contactsSlice';
-// import { useSelector } from 'react-redux';
-const ContactsList = ({ contacts }) => {
+const ContactsList = () => {
   const dispatch = useDispatch();
-  // const contacts = useSelector(state => state.contacts);
-  // const filter = useSelector(state => state.filter);
-  // const getFilterName = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-  //   return Object.values(contacts).filter(contact => {
-  //     if (contact.name)
-  //       return contact.name.toLowerCase().includes(normalizedFilter);
-  //   });
-  // };
+  const filter = useSelector(state => state.filter);
+  const contacts = useSelector(state => state.contacts);
   const handleDelContact = contactId => {
     dispatch(remove(contactId));
   };
   return (
     <ul className={css.contact__list}>
-      {Object.values(contacts).map(({ id, name, number }) => (
-        <li className={css.contact__item} key={id}>
-          <ContactListItem
-            // contacts={getFilterName()}
-            id={id}
-            name={name}
-            number={number}
-            onDelContact={handleDelContact}
-          />
-        </li>
-      ))}
+      {Object.values(contacts)
+        .filter(contact => {
+          if (contact.name)
+            return contact.name.toLowerCase().includes(filter.toLowerCase());
+        })
+        .map(({ id, name, number }) => (
+          <li className={css.contact__item} key={id}>
+            <ContactListItem
+              id={id}
+              name={name}
+              number={number}
+              onDelContact={handleDelContact}
+            />
+          </li>
+        ))}
     </ul>
   );
 };
